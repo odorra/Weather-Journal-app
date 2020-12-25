699d288d3f313e5cc431bea0e957e58e328bd1f6
odorra <72552566+odorra@users.noreply.github.com>
2020-12-25 17:13:51 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,5 +1,35 @@
 /* Global Variables */
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let d = newDate();
+//Take care that getMonth() counts month from 0 to 11 so you need to add 1 to its response 
+let newDate = d.getMonth()+ 1 + "."+ d.getDate()+'.'+ d.getFullYear();
+const getData = async () => {
+  //code to fetch data from api (await the fetch)
+  const request = await fetch(
+    baseUrl + `?zip=${zipInput.value}&apiid=${appKey}&units=metric`
+  );
+  try {
+    //code to convert json data and return result (await conversion)
+    const response = await request.json()
+    return response
+  } catch (error) {
+    //code to log error
+    console.log(error)
+  }
+};
+
+const postData = async (url = "", data {}) => {
+  //code to fetch route url and write request method, credentials, headers and body
+  const request = await fetch(url, {
+    "method": "POST"
+    "credentials": "same-origin",
+    "headers": {content.Type":"application/json"},
+    "body": JSON.stringify{data}
+    }}:
+  try {
+    return;
+  } catch (error) {
+    //code to log error
+  }
+};
