24cfb6e6010b45ed376006742ded7428b17c924b
odorra <72552566+odorra@users.noreply.github.com>
2020-12-25 18:26:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
+const port = 3333;
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(port, () => {console.log("server is running... listening to port $(port)");});
+                 
+     app.get('/getData', (req, res) => {
+    //replace url with a string of the get route url
+    //code to send res data of endpoint object
+    res.send(projectData);
+});
