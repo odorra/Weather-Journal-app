957c9080534dbb630ec9d34baed69d7c36ed0431
odorra <72552566+odorra@users.noreply.github.com>
2020-12-25 20:11:59 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,45 +1,46 @@
 /* Global Variables */
-
+const btn = document.getElementById("generate");
+const feeling = document.getElementById("feelings");
 // Create a new date instance dynamically with JS
-let d = newDate();
-let newDate = d.getMonth()+ 1 + "."+ d.getDate()+'.'+ d.getFullYear();
+let d = function newDate() {
+  newDate = d.getMonth()+ 1 + "."+ d.getDate()+'.'+ d.getFullYear()};
 let baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 //Take care that getMonth() counts month from 0 to 11 so you need to add 1 to its response 
 const getData = async () => {
   //code to fetch data from api (await the fetch)
   const request = await fetch(
-    baseUrl + `?zip=${zipInput.value}&apid=${4d685eb2ad2a8bd0a5b5707bf019b92c}&units=metric`
+    baseUrl + `?zip=${zipInput.value}&appid=$4d685eb2ad2a8bd0a5b5707bf019b92c&units=metric`
     );
-};
+
   try {
     //code to convert json data and return result (await conversion)
-    const response = await request.json()
-    return response
+    const call = async () => {
+      const response = await request.json()
+      return response}
   } catch (error) {
     //code to log error
     console.log(error)
   }
 };
 
-const postData = async (url = "", data {}) => {
+const postData = async function (url =" ", data = { }) {
   //code to fetch route url and write request method, credentials, headers and body
   const request = await fetch(url, {
-    "method": "POST"
+    "method": "POST",
     "credentials": "same-origin",
-    "headers": {content.Type":"application/json"},
-    "body": JSON.stringify{data}
-    }}:
+    "headers": { "content.Type": "application/json" },
+    "body": JSON.stringify
+  }, { data });
+};
   try {
-    return;
+    const call = async () => {
+      const response = await request.json()
+      return response}
   } catch (error) {
     //code to log error
     console.log(error)
   }
-app.post('url', (req, res) => {
-  //replace url with a string of the post route url
-  //code to add data to endpoint object
-  projectData.temp = req.body.temp
-})
+
 const updateUI = async () => {
     //code to fetch data from server. Use url from GET route (await the fetch)
     await fetch("/getData")
@@ -55,7 +56,13 @@ const updateUI = async () => {
 btn.addEventListener('click', performAction) 
 //Don't use parentheses for function inside event listener or it will not wait for event  
 function performAction() {
-   postData('/comment', {content:feeling.value});
+  postData('/postData', {content:feeling.value});
 };
 
-.then(() => updateUI());
+function result() {
+  fetch('/comment', settings)
+    .then(res => res.json())
+    .then((json) => {
+               entery.innerText = json.content;
+    });
+}  
